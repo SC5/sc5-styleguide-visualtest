9274ae7ba3d592c354d3f304f58659e7ed301b12
Varya Stepanova <mail@varya.me>
2015-10-14 17:31:17 +0300

    Simple taking the screenshots

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require("gulp");
 
+var sc5StyleguideGemini = require('./');
 var styleguide = require('sc5-styleguide');
 var concat = require('gulp-concat');
 
+var testDirPath = './tests/tmp';
 var styleguideSource = './tests/data/*.css';
 var styleguidePath = './tests/data/styleguide';
 
@@ -24,3 +26,23 @@ gulp.task('styleguide:applystyles', function() {
 });
 
 gulp.task('styleguide', ['styleguide:generate', 'styleguide:applystyles']);
+
+gulp.task("test:visual:config", ["styleguide"], function() {
+  gulp.src(styleguidePath, { read: false })
+    .pipe(sc5StyleguideGemini.configure({
+      excludePages: [
+        '2.2.1', // Back icon is not shown in prod
+        '6.1-2', // picture is not loaded in prod
+      ]
+    }))
+    .pipe(gulp.dest(testDirPath + '/config'))  // Path to configuration and tests
+});
+
+gulp.task("test:visual:update", ["test:visual:config"], function() {
+  gulp.src(styleguidePath, { read: false })
+    .pipe(sc5StyleguideGemini.gather({
+      configDir: testDirPath + '/config', // Path to configuration and tests
+      gridScreenshotsDir: testDirPath + '/grid-screenshots',
+      rootUrl: 'http://localhost:3000/'
+    }));
+});
